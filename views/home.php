7cb1eebd33a7653dda2f3aa80ceca38e769731a8
sadi debbal <debbal.sadi7@gmail.com>
2018-12-13 19:55:28 +0100

    wip gamelist

--- views/home.php
+++ views/home.php
@@ -4,17 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
+<header>
+<img src="img/logo.png">
+</header>
+<main>
     <ul>
     <?php
     foreach ($games as $game) {
     ?>
-        <li><img src="<?php echo $game["img_path"]?>"></li>
+    
+        <li>
+        <div class="container">
+            <img class="game" src="<?php echo $game["img_path"]?>">
+            <div class="bottom-left">
+                <h3> <?php echo $game["name"]?> </h3>
+            </div>  
+        </div>
+        </li>
+    
     <?php
     }
     ?>
     </ul>
+</main>
 </body>
 </html>
