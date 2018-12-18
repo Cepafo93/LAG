fed80a0ee4bc4016780eb0a1c9a6c3815f478d91
sadi debbal <debbal.sadi7@gmail.com>
2018-12-18 23:39:02 +0100

    wip front

--- views/game.php
+++ views/game.php
@@ -24,17 +24,79 @@
         coopératifs comme Tir Groupés 
         </p>
         </div>
-        <a href="#reservation"> <i class="fas fa-angle-down"></i> </a> 
         <h2> + de 100 coachs professionnels </h2>
     </div>
+    <a href="#disponibilites"> <i class="fas fa-angle-down"></i> </a> 
 </header>
 
 <main>
-<section id="reservation"> 
+<section>  
+<h1> Vos disponibilités </h1>
+<div id="disponibilites">
+<form> 
+    <div class="form">
+        <div id="calendrier">
+        
+        </div>
+        <div class="choixCoaching">
+            <div class="boutonsChoix">
+                <button class="buttonCoaching"> Suivre un coaching </button>
+                <button class="buttonCoaching"> Proposer un coaching </button> 
+            </div>
+
+            <div class="boutonsChoix">
+            <select class="selectCoaching">
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+            </select>
+
+            <select class="selectCoaching">
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+            </select>
+            </div>
+        </div>
+    </div>
+</form>
+</div>
+</section>
+<section id="coachs">
+<h1> Coachs </h1>
+<!-- <ul>
+    <?php
+    foreach ($games as $game) {
+    ?>
+        <li>
+        <div class="container">
+        <div>
+            <img class="game" src="<?php echo $game["img_path"]?>">
+            <div class="bottom-left">
+                <h3> <?php echo $game["name"]?> </h3>
+            </div>
+            <div class="middle">
+                <a href="#" class="gameButton"> Voir le jeu </a>
+            </div>
+        </div>
+        </div>
+        </li>
+    <?php
+    }
+    ?>
+    </ul>
+-->
 </section>
 </main>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="js/slick.min.js"> </script>
+<script type="text/javascript">
+//<![CDATA[
+calInit("calendrier", "", "champ_date", "jsCalendar", "day", "selectedDay");
+//]]>
+</script>
 </body>
 </html>
